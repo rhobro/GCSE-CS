5e80743b8bb9b6214e40bb57e2ae48ce0a79f07c
rhobro <robozmat@gmail.com>
2021-05-12 09:59:50 +0100

    Add files via upload

---
+++ latin/preprocess_wordlist.py
@@ -0,0 +1,27 @@
+import json
+from raw_vocab import *
+
+type_map = {
+    "v": "verb",
+    "n": "noun",
+    "a": "adjective",
+    "p": "preposition",
+    "r": "pronoun",
+    "d": "adverb",
+    "x": "misc"
+}
+
+new = []
+for w in vocab:
+    s = w.split("#")
+    n = {
+        "qLa": s[0],
+        "aLa": s[3].split(":"),
+        "qEn": s[1],
+        "aEn": s[4].split(":"),
+        "type": [type_map[t] for t in s[2]],
+        "stage": int(s[5])
+    }
+    new.append(n)
+
+print(json.dumps(new, indent="    "))
